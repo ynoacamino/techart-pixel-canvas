60829ffb8deb0dd7b874bb7b4708a0d91176452d
gustadev24 <lgsc21211@gmail.com>
2025-04-21 21:43:59 -0500

    Use port 8000 for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService: ConfigService = app.get(ConfigService);
+  const port = configService.get<number>('port') || 8000;
+  await app.listen(port);
 }
 bootstrap();
