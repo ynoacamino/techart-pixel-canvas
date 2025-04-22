d88a8d3c885943cb483e29beb52b2ef13c8568f7
gustadev24 <lgsc21211@gmail.com>
2025-04-21 21:43:59 -0500

    Use port 8000 for backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 import { BoardModule } from './board/board.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [BoardModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
+    BoardModule, PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
