1f4d375258ed84ffee1196bebc3cb18c0413fd94
gustadev24 <lgsc21211@gmail.com>
2025-04-22 19:25:40 -0500

    Logout button on right side

--- frontend/src/components/auth/Auth.tsx
+++ frontend/src/components/auth/Auth.tsx
@@ -12,34 +12,36 @@ export default function Auth({
   user,
 }: AuthProps) {
   return (
-    <div className="flex flex-col gap-y-4">
+    <div className="flex flex-col gap-y-4 items-center">
       <p>
         {user ? '¡Bienvenido de nuevo!' : 'Inicia sesión para pintar'}
       </p>
-      {
-        user && (
-          <div className="flex gap-3">
-            {
-              user.img ? (
-                <img
-                  src={user.img}
-                  alt="Perfil"
-                  className="size-10 rounded-full"
-                />
-              ) : (
-                <div className="size-10 rounded-full bg-gray-500" />
-              )
-            }
-            <div className="flex flex-col">
-              <p className="text-lg font-bold">{user.name}</p>
-              <p className="text-sm font-light text-gray-700">{user.email}</p>
+      <div className="flex gap-x-20 items-center">
+        {
+          user && (
+            <div className="flex gap-x-3 items-center">
+              {
+                user.img ? (
+                  <img
+                    src={user.img}
+                    alt="Perfil"
+                    className="size-10 rounded-full"
+                  />
+                ) : (
+                  <div className="size-10 rounded-full bg-gray-500" />
+                )
+              }
+              <div className="flex flex-col">
+                <p className="text-lg font-bold">{user.name}</p>
+                <p className="text-sm font-light text-gray-700">{user.email}</p>
+              </div>
             </div>
-          </div>
-        )
-      }
-      <Button size="sm">
-        {user ? 'Cerrar Sesión' : 'Iniciar Sesión'}
-      </Button>
+          )
+        }
+        <Button size="sm">
+          {user ? 'Cerrar Sesión' : 'Iniciar Sesión'}
+        </Button>
+      </div>
     </div>
   );
 }
