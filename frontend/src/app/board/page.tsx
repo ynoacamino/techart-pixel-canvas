a513e61733e54fae88298bf1255df720a68e113d
gustadev24 <lgsc21211@gmail.com>
2025-04-23 08:39:32 -0500

    Add color picker component on Board page

--- frontend/src/app/board/page.tsx
+++ frontend/src/app/board/page.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 import Board from '@/components/pages/canvas/board';
+import ColorPicker from '@/components/pages/canvas/colorPicker';
 import Zoom from '@/components/ui/zoom';
 
 export default function Home() {
   return (
-    <Zoom>
-      <div className="w-screen h-screen bg-zinc-300 flex items-center justify-center">
-        <Board />
-      </div>
-    </Zoom>
+    <>
+      <Zoom>
+        <div className="w-screen h-screen bg-zinc-300 flex items-center justify-center">
+          <Board />
+        </div>
+      </Zoom>
+      <ColorPicker />
+    </>
   );
 }
