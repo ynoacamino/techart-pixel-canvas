1bfaa79c9a54c2f97c0a438b0c1aeb3cb73cba99
gustadev24 <lgsc21211@gmail.com>
2025-04-23 15:21:00 -0500

    Crate Google strategy

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,3 +1,10 @@
-export default () => ({
-  port: parseInt(process.env.PORT || "8080", 10),
-});
+export default () => {
+  const backendUrl = process.env.BACKEND_URL || 'http://localhost:3000';
+  return {
+    env: process.env.NODE_ENV || 'development',
+    port: parseInt(process.env.PORT || "8080", 10),
+    clientId: process.env.CLIENT_ID || '',
+    clientSecret: process.env.CLIENT_SECRET || '',
+    callbackURL: `${backendUrl}/auth/callback`,
+  }
+};
