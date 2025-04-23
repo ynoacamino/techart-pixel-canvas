e0843128263dc87f1a46dff35c0479eaeb616a05
gustadev24 <lgsc21211@gmail.com>
2025-04-23 18:30:12 -0500

    Add imports in auth module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { GoogleStrategy } from './google.strategy';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'src/config/configuration';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService]
+  imports: [UsersModule, PassportModule, ConfigModule.forRoot({ load: [configuration] })],
+  providers: [AuthService, GoogleStrategy],
+  controllers: [AuthController]
 })
 export class AuthModule {}
