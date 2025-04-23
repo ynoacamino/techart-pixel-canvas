71e705893cb7b846f4ad28ef14d64a5d1349a7e3
gustadev24 <lgsc21211@gmail.com>
2025-04-22 19:49:24 -0500

    Add gallery template

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Auth from '@/components/auth/Auth';
+import Gallery from '@/components/pages/home/Gallery';
 import Hero from '@/components/pages/home/Hero';
 
 const user = {
@@ -11,6 +12,8 @@ export default function Home() {
     <div className="min-h-screen w-full flex flex-col gap-y-6 py-6 items-center justify-center">
       <Hero />
       <Auth user={user} />
+      <Gallery side="left" />
+      <Gallery side="right" />
     </div>
   );
 }
