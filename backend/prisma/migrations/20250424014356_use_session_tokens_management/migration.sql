a11c1d2a58fdde7b6c600d6fe859ae3c8e02afac
gustadev24 <lgsc21211@gmail.com>
2025-04-23 20:58:09 -0500

    Prepare database for sessionToken management

---
+++ backend/prisma/migrations/20250424014356_use_session_tokens_management/migration.sql
@@ -0,0 +1,28 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `accessToken` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `revoked` on the `Session` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Session` table. All the data in the column will be lost.
+  - Added the required column `sessionToken` to the `Session` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Session" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" INTEGER NOT NULL,
+    "sessionToken" TEXT NOT NULL,
+    "userAgent" TEXT,
+    "ipAddress" TEXT,
+    "expiresAt" DATETIME NOT NULL,
+    CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Session" ("expiresAt", "id", "ipAddress", "userAgent", "userId") SELECT "expiresAt", "id", "ipAddress", "userAgent", "userId" FROM "Session";
+DROP TABLE "Session";
+ALTER TABLE "new_Session" RENAME TO "Session";
+CREATE UNIQUE INDEX "Session_sessionToken_key" ON "Session"("sessionToken");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
