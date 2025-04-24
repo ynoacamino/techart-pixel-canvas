69d09999ebfc254d72c998d462054a97fced1973
gustadev24 <lgsc21211@gmail.com>
2025-04-24 10:03:03 -0500

    Create swr hooks to user requests

---
+++ frontend/src/hooks/swr.tsx
@@ -0,0 +1,13 @@
+import { api } from '@/lib/api';
+import { User } from '@/lib/models';
+import useSWR from 'swr';
+
+export const useUser = () => {
+  const { data, error } = useSWR<User, Error>('/api/auth/me', () => api.getUser());
+
+  return {
+    user: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
