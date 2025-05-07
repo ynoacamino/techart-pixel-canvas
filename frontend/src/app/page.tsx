ed21ab91ac5d9a72802a49e7017a1a507a8815c8
ynoacamino <ynoacamino@gmail.com>
2025-05-06 23:50:26 -0500

    remove sroll bar in board page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,16 +4,19 @@ import Auth from '@/components/auth/Auth';
 import Gallery from '@/components/pages/home/Gallery';
 import Hero from '@/components/pages/home/Hero';
 import Sponsors from '@/components/pages/home/Sponsors';
+import { BarScroll } from '@/components/ui/barscroll';
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-background flex flex-col items-center w-full">
+      <BarScroll side="left" />
+      <BarScroll side="right" />
       <div className="flex flex-col gap-y-6 mb-20">
         <Hero />
         <Auth />
       </div>
       <Sponsors />
       <Gallery />
-    </>
+    </div>
   );
 }
